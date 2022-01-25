ce92427970dd1c23a2d3dcf14f276dc34349e674
sofi_tissera <sofiatissera@yahoo.com.ar>
2022-01-24 23:06:57 -0300

    commit2

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ const header = document.querySelector('.main-header');
 
 window.addEventListener('scroll', () => {
     const scrollPos = window.scrollY;
-    if(scrollPos > 10){
+    if (scrollPos > 10) {
         header.classList.add('scrolled');
     } else {
         header.classList.remove('scrolled')
